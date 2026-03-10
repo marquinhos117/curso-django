f681202bc52834c2f15aec6320980815c4a43c40
Marcos Eduardo <emarcos117@outlook.com>
2026-03-09 21:17:27 -0300

    Foi adicionado um namespace, herança, bocos, condições e laços de repetição juntamente com a correção da pasta static, criação da pasta template global estyle, dentre outras melhorias. Foi criado atambém um script para gerar dados ficticios para testes

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.urls import path
 from django.http import HttpResponse
+from recipes.utils.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Marcos Eduardo',
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
 
