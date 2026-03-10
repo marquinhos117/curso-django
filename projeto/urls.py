cec95f94a5c366e4e1d11a5e98499425f8112d21
Marcos Eduardo <emarcos117@outlook.com>
2026-03-10 00:37:12 -0300

    Configurando a media url e testes realizados no django admin

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 # http request
@@ -26,6 +28,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls'))
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
